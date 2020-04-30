cb6d7abd209d4a88e000aca68bd3506337583799
Felipe Rocha <felipemota.rocha@gmail.com>
2020-04-30 17:14:06 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import GoogleLogin from "react-google-login";
 
 function App() {
+	const [name, setName] = useState();
+	const [email, setEmail] = useState();
+	const [profilePic, setProfilePic] = useState();
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	const responseGoogle = (response) => {
+		console.log(response);
+		const {
+			profileObj: { name, email, imageUrl },
+		} = response;
+		setName(name);
+		setEmail(email);
+		setProfilePic(imageUrl);
+		setIsLoggedIn(true);
+	};
 	return (
-		<div>
-			<h1>hello world!</h1>
+		<div className="container">
+			<GoogleLogin
+				clientId="366023343940-ndamikr6o5em3sivatijihspjsg8gmt2.apps.googleusercontent.com"
+				buttonText="Continuar com o Google"
+				onSuccess={responseGoogle}
+				onFailure={responseGoogle}
+			/>
+			{isLoggedIn ? (
+				<div style={{ textAlign: "center" }}>
+					<h1>User Information</h1>
+					<img className="profile" src={profilePic} alt="Profile" />
+					<p>Name: {name}</p>
+					<p>Email: {email}</p>
+				</div>
+			) : (
+				""
+			)}
 		</div>
 	);
 }
